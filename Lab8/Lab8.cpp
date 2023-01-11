e5eb7b42b08878af7264d9b911c87f06d7fad975
Yevhenii Grytsay <60937148+sultan2131@users.noreply.github.com>
2023-01-11 21:39:57 +0200

    Add files via upload

---
+++ Lab8/Lab8.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n <= 1)  
+    {
+        return 1;
+    }
+    return n * factorial(n - 1); 
+}
+void reverseBinary(int n)
+{
+    if (n == 0) 
+    {
+        return;
+    }
+    cout << n % 2;
+    reverseBinary(n / 2); 
+}
+
+void forvardBinary(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    forvardBinary(n / 2); 
+    cout << n % 2;
+}
+
+
+void ReverseForvardBinary(int n)
+{
+    if (n == 0) 
+    {
+        return;
+    }
+    cout << n % 2; 
+    ReverseForvardBinary(n / 2); 
+    cout << n % 2; 
+}
+
+
+int product(int x, int y)
+{
+    if (y == 0) 
+    {
+        return 0;
+    }
+    return (x + product(x, y - 1)); 
+}
